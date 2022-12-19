e827f043ae6d42c786612282be3fae893bbf31dc
Shah, Monarch {MDBU~South San Francisco} <shah.monarch@gene.com>
2022-12-19 19:34:08 +0100

    update day8 2016

--- 2016/day8.R
+++ 2016/day8.R
@@ -8,35 +8,57 @@ library(tidyverse)
 # Read Puzzle Input
 day8_data <- read.table(file = "~/AoC/2016/data/day8_data", sep=",")
 
+rect_fun <- function(rect, row, col) {
+  rect[seq_len(row), seq_len(col)] <- 1
+  return(rect)
+}
+
+rotate_fun <- function(x, by) {
+  len <- length(x)
+  x[c(seq(len - by + 1, len), seq(1, len -by))]
+}
+
+rotate_column <- function(rect, column, by) {
+  rect[, column] <- rotate_fun(rect[, column], by)
+  return(rect)
+}
+
+rotate_row <- function(rect, row, by) {
+  rect[row, ] <- rotate_fun(rect[row, ], by)
+  return(rect)
+}
+
 # Screen is 50 pixel wide and 6 pixel tall
 
 screen <- matrix(0, 6, 50)
-input <- day8_data$V1[1:4]
+input <- day8_data$V1
+input <- c("rect 3x2", "rotate column x=1 by 1", "rotate row y=0 by 4", "rotate column x=1 by 1")
 
 for (i in 1:length(input)) {
   if (str_detect(input[i], "rect")) {
     
     # Turn on the top left ZxZ grid
-    col <- str_extract_all(input[i], "[0-9]+", simplify = TRUE)[,1]
-    row <- str_extract_all(input[i], "[0-9]+", simplify = TRUE)[,2]
+    col <- as.numeric(str_extract_all(input[i], "[0-9]+", simplify = TRUE)[,1])
+    row <- as.numeric(str_extract_all(input[i], "[0-9]+", simplify = TRUE)[,2])
     
-    screen[1:row, 1:col] <- 1 
+    screen <- rect_fun(screen, row, col)
+    
   }
   else if (str_detect(input[i], "rotate")) {
     
     if (str_detect(input[i], "row")) {
-      rownum <- str_extract_all(input[i], "[0-9]+", simplify = TRUE)[,1]
-      rowby <- str_extract_all(input[i], "[0-9]+", simplify = TRUE)[,2]
+      rownum <- as.numeric(str_extract_all(input[i], "[0-9]+", simplify = TRUE)[,1]) + 1
+      rowby <- as.numeric(str_extract_all(input[i], "[0-9]+", simplify = TRUE)[,2])
       
-      print(rownum)
-      print(rowby)
+      screen <- rotate_row(screen, rownum, rowby) 
+      
     }
     else if (str_detect(input[i], "column")) {
-      colnum <- str_extract_all(input[i], "[0-9]+", simplify = TRUE)[,1]
-      colby <- str_extract_all(input[i], "[0-9]+", simplify = TRUE)[,2]
+      colnum <- as.numeric(str_extract_all(input[i], "[0-9]+", simplify = TRUE)[,1]) + 1
+      colby <- as.numeric(str_extract_all(input[i], "[0-9]+", simplify = TRUE)[,2])
       
-      print(colnum)
-      print(colby)
+      screen <- rotate_column(screen, colnum, colby)
+      
     }
   }
 }
