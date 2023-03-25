af9a324b867c5bed462dd877880303bfe0af2a2f
Monarch Shah <monarch83@gmail.com>
2023-03-24 19:26:46 -0700

    day1 2022 viz

--- 2022/day1.R
+++ 2022/day1.R
@@ -5,9 +5,13 @@
 # Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
 
 library(stringr)
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(gganimate)
 
 # Read Puzzle Input
-day1_data <- read.table(file = "~/AoC/2022/data/day1_data", blank.lines.skip=F)
+day1_data <- read.table(file = "AoC/2022/data/day1_data", blank.lines.skip=F)
 
 # Test case
 # day1_data <- data.frame(c(NA, 1000, 2000, 3000, NA, 4000, NA, 5000, 6000, NA, 7000, 8000, 9000, NA, 10000))
@@ -33,3 +37,59 @@ day1_totcal_sort <- sort(day1_totcal, decreasing = TRUE)
 
 cat("Find the top three Elves carrying the most Calories: ", names(day1_totcal_sort[1:3]), "\n",
     "How many total Calories top 3 elfes are Carring: ", as.numeric(sum(day1_totcal_sort[1:3])))
+
+# Viz
+
+df <- data.frame()
+for (i in 1:length(day1_dlist)) {
+  
+  df <- bind_rows(df, data.frame(t(sapply(day1_dlist[[i]],c))))
+  
+}
+
+df <- df %>%
+  select(-X1) %>%
+  mutate(elf = row_number()) 
+  
+df$sum <- rowSums(df[, 1:15], na.rm = TRUE)
+
+df <- df %>% 
+  unite(., col = "New.Col", X2:X16, na.rm=TRUE, sep = ",")
+
+df <- df %>%
+  mutate(colcat = 1,
+         sumcat = 1.4)
+
+df <- df %>%
+  mutate(maxcal = ifelse(sum == max(df$sum), max(df$sum), NA))
+
+g <- ggplot(df, aes(colcat, elf, label = New.Col)) +
+  geom_text(size = 1, hjust = 0) +
+  scale_y_continuous(breaks = seq(1, 256, by=1), limits = c(1,256)) +
+  xlim(1, 1.5) 
+  
+g <- g +
+  geom_text(aes(sumcat, elf, label = sum), size = 1, hjust = 0, color = "blue") +
+  geom_text(aes(sumcat, elf, label = maxcal), size = 1, hjust = 0, color = "red") +
+  theme(axis.text.y = element_text(size=3),
+        axis.text.x = element_blank(),
+        panel.background = element_blank(),
+        panel.border = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.title.x = element_blank(),
+        axis.ticks.x = element_blank()
+        ) 
+
+g
+
+pdf(file="AoC/2022/gifs_viz/day1.pdf",
+    width = 8,
+    height=22,
+    bg = "white",
+    paper = "legal")
+plot(g)
+dev.off()
+
+g <- g +
+  transition_reveal(elf) 
