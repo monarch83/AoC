7ae61f91acfd4a0aab7d7ae1dedbd14b88124e42
Monarch Shah <monarch83@gmail.com>
2023-05-04 07:36:46 -0700

    AoC 2018 Day 1

--- 2018/day1.R
+++ 2018/day1.R
@@ -22,13 +22,13 @@ start_time <- Sys.time()
 
 for (i in 2:length(freq)) {
   #print(day1_data$csum[i] == day1_data$csum[1:(i-1)])
-  if (any(freq_csum[i] == freq_csum[1:(i-1)])) { 
-    cat("What is the first frequency your device reaches twice? \n", 
-        "Iteration: ", ceiling(i/length(day1_data$V1)), "\n", 
+  if (any(freq_csum[i] == freq_csum[1:(i-1)])) {
+    cat("What is the first frequency your device reaches twice? \n",
+        "Iteration: ", ceiling(i/length(day1_data$V1)), "\n",
         "Frequency: ", freq_csum[i], "\n")
     break
   }
-  
+
 }
 
 end_time <- Sys.time()
