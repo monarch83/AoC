b4deb98aff548f73630bca884d3b6e0cd5dff485
Shah, Monarch {MDBU~South San Francisco} <shah.monarch@gene.com>
2023-11-29 18:26:27 +0100

    2020 day 1

--- 2020/day1.R
+++ 2020/day1.R
@@ -2,6 +2,7 @@
 # Day 1  Report Repair ---
 
 library(stringr)
+library(dplyr)
 
 # Read Puzzle Input
 day1_data <- read.table(file = "AoC/2020/data/day1_data")
@@ -45,3 +46,24 @@ for (i in 1:length(day1_data)) {
   }
   
 }
+
+# Solution 2 ----
+
+day1_2 <- expand.grid(day1_data, day1_data)
+day1_2 <- day1_2 %>%
+  mutate(fl_2020 = ifelse(Var1 + Var2 == 2020, 1, 0)) %>%
+  filter(fl_2020 == 1)
+
+cat(paste0("The two entries that sum to 2020 are ",
+           day1_2$Var1[1], " and ", day1_2$Var2[1], ". Multiplying them together produces ",
+           day1_2$Var1[1] * day1_2$Var2[1]))
+
+day1_21 <- expand.grid(day1_data, day1_data, day1_data)
+
+day1_21 <- day1_21 %>%
+  mutate(fl_2020 = ifelse(Var1 + Var2 + Var3 == 2020, 1, 0)) %>%
+  filter(fl_2020 == 1)
+
+cat(paste0("The three entries that sum to 2020 are ",
+           day1_21$Var1[1], day1_21$Var2[1], " and ", day1_21$Var3[1], ". Multiplying them together produces ",
+           day1_21$Var1[1] * day1_21$Var2[1] * day1_21$Var3[1]))
